389312658a2a4755c1084c46d0c7dce059327e61
salenesic79-gif <salenesic79@gmail.com>
2025-08-17 18:14:09 +0200

    Update urls.py

--- tovar_taxi/urls.py
+++ tovar_taxi/urls.py
@@ -4,3 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
 ]
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('taxi_app.urls')),  # tvoja aplikacija na glavnom URL-u
+]
