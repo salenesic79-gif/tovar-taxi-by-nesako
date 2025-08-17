281d34f5e08f8a0b92f76f6c064a703ef98d59d7
salenesic79-gif <salenesic79@gmail.com>
2025-08-17 18:55:14 +0200

    Update views.py

--- tovar_taxi/taxi/views.py
+++ tovar_taxi/taxi/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("<h1>Dobrodošao u Taxi projekat!</h1><p>Server radi!</p>")
+from django.http import HttpResponse
+
+def lista_tovara(request):
+    return HttpResponse("Ovo je lista svih tovarnih jedinica.")
+
+def detalji_tovara(request, tovar_id):
+    return HttpResponse(f"Detalji tovara sa ID-jem: {tovar_id}")
