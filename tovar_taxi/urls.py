6b1617ba322dc82622dc2105ca5119ac619d9656
salenesic79-gif <salenesic79@gmail.com>
2025-08-18 19:48:23 +0200

    Create urls.py

---
+++ tovar_taxi/urls.py
@@ -0,0 +1,8 @@
+# tovar_taxi/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('transport.urls')),
+]
