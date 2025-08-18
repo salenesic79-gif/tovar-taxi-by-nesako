ef63bc49d412a0a60700a6af1261a2235a4ccb3c
salenesic79-gif <salenesic79@gmail.com>
2025-08-18 20:23:48 +0200

    Update settings.py

--- tovar_taxi/settings.py
+++ tovar_taxi/settings.py
@@ -107,5 +107,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/orders/'
+LOGIN_REDIRECT_URL = '/orders/'
