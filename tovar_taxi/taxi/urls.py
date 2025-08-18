b39b7abe3673e67b6bd343895a19755b033121f0
salenesic79-gif <salenesic79@gmail.com>
2025-08-18 19:36:46 +0200

    Delete tovar_taxi directory

--- tovar_taxi/taxi/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from tovar import views  # Pretpostavljam da je ime vaše aplikacije 'tovar'
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('tovar/', views.lista_tovara, name='lista_tovara'),
-    path('tovar/<int:tovar_id>/', views.detalji_tovara, name='detalji_tovara'),
-]
