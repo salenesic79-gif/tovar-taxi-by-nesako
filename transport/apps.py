4106f8fa9aeba86128a22da7812a2fd9cd01a9a8
salenesic79-gif <salenesic79@gmail.com>
2025-08-20 21:26:42 +0200

    Update apps.py

--- transport/apps.py
+++ transport/apps.py
@@ -1,19 +1,8 @@
-# transport/apps.py
 from django.apps import AppConfig
 
 class TransportConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'transport'
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.contrib.auth.models import User
-from .models import Profile
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    def ready(self):
+        import transport.signals  # samo uvoz, ne definicija signala
