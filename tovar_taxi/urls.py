1914e341ff8ec34bea8151dcc1af4de35fafa426
salenesic79-gif <salenesic79@gmail.com>
2025-08-20 21:09:47 +0200

    Update urls.py

--- tovar_taxi/urls.py
+++ tovar_taxi/urls.py
@@ -1,8 +1,8 @@
-# tovar_taxi/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('transport.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),  # login, logout
 ]
