51a3bf2254fcfb4a3fc4a2ecfa1cf1b664f102ee
salenesic79-gif <salenesic79@gmail.com>
2025-08-21 21:33:39 +0200

    Update settings.py

--- tovar_taxi/settings.py
+++ tovar_taxi/settings.py
@@ -19,14 +19,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'transport.apps.TransportConfig',
+    'transport.apps.TransportConfig',  # <-- Ova linija je obavezna
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
