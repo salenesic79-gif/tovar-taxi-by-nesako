d6b0c51444e5db952375196d8e6d56eff39f1310
salenesic79-gif <salenesic79@gmail.com>
2025-08-21 20:17:49 +0200

    Update urls.py

--- transport/urls.py
+++ transport/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.user_signup, name='signup'),
-    path('order/create/', views.order_create, name='order_create'),
-    path('vehicle/create/', views.vehicle_create, name='vehicle_create'),
+    path('vehicles/', views.vehicle_list, name='vehicle_list'),
+    path('vehicles/add/', views.vehicle_create, name='vehicle_create'),
+    path('orders/', views.order_list, name='order_list'),
+    path('orders/add/', views.order_create, name='order_create'),
 ]
