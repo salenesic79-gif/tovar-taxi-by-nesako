4239d2b2d65586bdae59dc633f4f9de8f610613d
salenesic79-gif <salenesic79@gmail.com>
2025-08-21 20:15:44 +0200

    Update forms.py

--- transport/forms.py
+++ transport/forms.py
@@ -1,12 +1,25 @@
 from django import forms
-from .models import Vehicle, Order
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Vehicle, Order, Profile
+
+class CustomUserCreationForm(UserCreationForm):
+    ROLE_CHOICES = (
+        ('client', 'Client'),
+        ('driver', 'Driver'),
+    )
+    role = forms.ChoiceField(choices=ROLE_CHOICES)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2', 'role']
 
 class VehicleForm(forms.ModelForm):
     class Meta:
         model = Vehicle
-        fields = ['license_plate', 'capacity', 'status']
+        fields = ['name', 'plate_number', 'capacity', 'available']
 
 class OrderCreationForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['origin', 'destination', 'cargo_description', 'status']
+        fields = ['origin', 'destination', 'cargo_description', 'created_at', 'status', 'customer', 'vehicle']
