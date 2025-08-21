0848231db497ccef27bbf6899a08df11607c7dc7
salenesic79-gif <salenesic79@gmail.com>
2025-08-21 20:54:50 +0200

    Update models.py

--- transport/models.py
+++ transport/models.py
@@ -1,20 +1,10 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
+from django.contrib.auth.models import User
+from django.db import models
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tovar_taxi.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20, blank=True)
+    address = models.CharField(max_length=255, blank=True)
 
-if __name__ == '__main__':
-    main()
+    def __str__(self):
+        return f"{self.user.username} Profile"
