9023d5c52178ed6939ee236b4c1ab259aab738ce
salenesic79-gif <salenesic79@gmail.com>
2025-08-21 21:01:05 +0200

    Update urls.py

--- transport/urls.py
+++ transport/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('signup/', views.user_signup, name='signup'),
-    path('vehicles/', views.vehicle_list, name='vehicle_list'),
-    path('vehicles/add/', views.vehicle_create, name='vehicle_create'),
-    path('orders/', views.order_list, name='order_list'),
-    path('orders/add/', views.order_create, name='order_create'),
+    path('', views.home_view, name='home'),
+    path('signup/', views.signup_view, name='signup'),
 ]
