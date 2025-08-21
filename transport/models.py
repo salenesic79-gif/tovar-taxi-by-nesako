fab3ed65068a115b175ee539c90065a7c4194721
salenesic79-gif <salenesic79@gmail.com>
2025-08-21 21:46:22 +0200

    Update models.p

--- transport/models.py
+++ transport/models.py
@@ -1,10 +1,11 @@
-from django.contrib.auth.models import User
+
 from django.db import models
+from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=20, blank=True)
-    address = models.CharField(max_length=255, blank=True)
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=255)
 
     def __str__(self):
-        return f"{self.user.username} Profile"
+        return self.user.username
