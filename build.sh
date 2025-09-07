332d5a9fe1075dc107e1a4e49b9c66f73789c811
salenesic79-gif <salenesic79@gmail.com>
2025-09-07 09:21:33 +0200

    Fix build.sh script i ažuriraj Stripe verziju

--- build.sh
+++ build.sh
@@ -5,8 +5,11 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Collect static files
-python manage.py collectstatic --no-input
+# Create migrations
+python manage.py makemigrations
 
 # Run migrations
 python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --no-input
